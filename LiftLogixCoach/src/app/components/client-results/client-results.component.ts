86d4a688441c5611426346d8ed5c77ee4db15800
Norbert Klessen <norbet.klessen@gmail.com>
2024-08-03 13:48:17 +0200

    adding new client result

--- LiftLogixCoach/src/app/components/client-results/client-results.component.ts
+++ LiftLogixCoach/src/app/components/client-results/client-results.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ClientsComponent} from "../clients/clients.component";
 import {ResultService} from "../../services/result.service";
 import {Result} from "../../interfaces/Result";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-client-results',
@@ -12,9 +13,14 @@ export class ClientResultsComponent implements OnInit{
   @Input() clientId: number | null = null;
   currentResult: Result | null = null;
 
+  benchpress: number | null = null;
+  deadlift: number | null = null;
+  squat: number | null = null;
+
   constructor(
     private clientsComponent: ClientsComponent,
-    private resultService: ResultService
+    private resultService: ResultService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -40,7 +46,46 @@ export class ClientResultsComponent implements OnInit{
     };
   }
 
-  goBack(event: Event) {
+  addResult() {
+    if (this.clientId === null) {
+      this.openSnackBar('Nieprawidłowe id klietna');
+      return
+    }
+
+    const token = localStorage.getItem('token') || '';
+    this.resultService.addResult(this.clientId, token, this.benchpress, this.deadlift, this.squat)
+      .subscribe({
+        next: (result) => {
+          this.openSnackBar('Result added successfully!');
+          this.benchpress = null;
+          this.deadlift = null;
+          this.squat = null;
+          if (this.clientId !== null) {
+            this.loadCurrentResult(this.clientId);
+          }
+        },
+        error: (error) => {
+          if (error.status = 400) {
+            this.openSnackBar('Wprowadź przynajmiej jeden wynik');
+          } else {
+            this.openSnackBar('Nie udało sie wprowadzić wyniku');
+          }
+        }
+      });
+  }
+
+  private openSnackBar(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      verticalPosition: 'top'
+    });
+  }
+
+  stopPropagation(event: Event): void {
+    event.stopPropagation();
+  }
+
+  goBack(event: Event): void {
     event.stopPropagation();
     this.clientsComponent.clearSelectedComponent();
   }
